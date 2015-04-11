0945f4a41d00cdd9beeef4a36f87f755ac9afca2
shubags <jason.w.shu@gmail.com>
2015-04-11 12:21:05 -0700

    Changed precision of hover-over boxes

--- HW1/countLetters.js
+++ HW1/countLetters.js
@@ -96,7 +96,7 @@ var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    return "<strong>Frequency:</strong> <span style='color:red'>" + d.value + "</span>";
+    return "<strong>Frequency:</strong> <span style='color:red'>" + d.value.toFixed(4) + "</span>";
   })
 
 
